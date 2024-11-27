9d5dd446f5892e88d0c7374712db5ce6fe0d0766
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-11-27 07:47:40 +0200

    added xray

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const AWS = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk');
+
+AWSXRay.captureAWS(AWS);
+
 const Task = require("./services/task");
 
 const { Consumer } = require("sqs-consumer");
@@ -6,10 +11,16 @@ const logger = require("./utils/logger");
 const consumer = Consumer.create({
     queueUrl: "https://sqs.eu-west-1.amazonaws.com/569985934894/tsk-dev-tasks",
     handleMessage: async (message) => {
+        const segment = AWSXRay.getSegment();
+        const subsegment = segment.addNewSubsegment("clientTaskPerform")
+
         const { Body } = message;
+
         const payload = JSON.parse(Body);
+        subsegment.addMetadata("taskDetails", payload);
         const task = new Task(payload);
         await task.perform();       
+        subsegment.close();
     },
 });
 
@@ -21,6 +32,6 @@ consumer.on("processing_error", (err) => {
     console.error(err.message);
 });
 
-consumer.start();
 logger.info("Registered to SQS and wait for messages");
+consumer.start();
 
