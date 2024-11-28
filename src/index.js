09a7ddff973bb78d959a6137125f2e2b3d846531
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-11-28 03:24:32 +0200

    fixed: xray

--- src/index.js
+++ src/index.js
@@ -10,27 +10,25 @@ const { Consumer } = require("sqs-consumer");
 const consumer = Consumer.create({
     queueUrl: process.env.TASKS_QUEUE_URL,
     handleMessage: async (message) => {
+        const { traceHeader, taskBody } = JSON.parse(Body);
+        const segment = new AWSXRay.Segment(`task-runner-${process.env.ENVIRONMENT}`, traceHeader);
         try {
-
             const { Body } = message;
-            logger.info("Received message", { Body })
-
-            const { traceHeader, taskBody } = JSON.parse(Body);
-            const segment = new AWSXRay.Segment("TaskProcessing", traceHeader);
-            console.log({aws: AWSXRay, segment, traceHeader})
+            logger.info("Received message", { Body });            
+            console.log({aws: AWSXRay, segment, trace})
             segment.addAnnotation("Environment", process.env.ENVIRONMENT);
             segment.addAnnotation("TaskId", taskBody.id);
             segment.addMetadata("QueueMessage", taskBody);
 
             const task = new Task(taskBody);
             await task.perform();
+            segment.close();
         } catch (err) {
             logger.error(`Received error: ${err.message}`, { error: err.message });
             throw err;
         } finally {
             segment.close();
         }
-
     },
 });
 
